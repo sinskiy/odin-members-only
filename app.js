61a850bb5e51bbccbe0ff21dea34cc704b7ca34a
sinskiy <sinskiy@proton.me>
2024-08-21 19:41:52 +0300

    fix: handle req.body correctly

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("node:path");
 
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
