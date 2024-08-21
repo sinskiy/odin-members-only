bb07f94bdfa0a7a7168359c63e4e3bc5f5b7db71
sinskiy <sinskiy@proton.me>
2024-08-21 19:12:56 +0300

    init: setup views

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+const path = require("node:path");
 
 const express = require("express");
-
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index", { title: "Home" }));
 
 app.listen(process.env.PORT, () =>
   console.log(`http://${process.env.HOSTNAME}:${process.env.PORT}`),
