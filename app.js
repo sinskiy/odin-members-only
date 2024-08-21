325e6dff058add3a562e7a1898924b8fb2391e41
sinskiy <sinskiy@proton.me>
2024-08-21 19:41:10 +0300

    init: setup public and reset.css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.render("index", { title: "Home" }));
 
