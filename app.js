68de59d355967c4b5e9ef135eb15a3dd4818bd67
sinskiy <sinskiy@proton.me>
2024-08-21 19:08:54 +0300

    init: setup basic server

---
+++ app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.listen(process.env.PORT, () =>
+  console.log(`http://${process.env.HOSTNAME}:${process.env.PORT}`),
+);
